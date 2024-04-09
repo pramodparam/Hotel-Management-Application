7dd4e8518b7b4fbc9b7a32cdbbe855e99b7d2d9f
Pramod R <81947919+pramodparam@users.noreply.github.com>
2024-04-09 20:43:45 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -5,7 +5,15 @@ const dbConfig=require('./db');
 const roomsRoute=require('./routes/roomRoutes')
 const userRoute=require('./routes/userRoutes')
 const bookingRoute=require('./routes/bookingRoutes')
+let cors = require("cors");
+app.use(cors());
 app.use(express.json())
+app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
+    next(); 
+})
 app.use('/api/rooms', roomsRoute)
 app.use('/api/users', userRoute)
 app.use('/api/bookings', bookingRoute)
